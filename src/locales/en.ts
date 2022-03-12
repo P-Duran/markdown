0f54079d24f1a8636b38d298804178abca0220f8
Elias Iglesias <eliasiglesiasesp@gmail.com>
2022-03-12 13:53:24 +0100

    home page

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -2,6 +2,7 @@ export const en = {
   translations: {
     //   Menu Items Labels
     "menuItem.home": "Home",
+    "menuItem.dashboard": "Dashboard",
     "menuItem.editor": "Editor",
   },
 };
--- src/locales/es.ts
+++ src/locales/es.ts
@@ -2,6 +2,7 @@ export const es = {
   translations: {
     //   Menu Items Labels
     "menuItem.home": "Inicio",
+    "menuItem.dashboard": "Dashboard",
     "menuItem.editor": "Editor",
   },
 };
