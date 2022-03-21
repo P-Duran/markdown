8f52de150bca49673d2b033f450eb268df042769
Pablo Durán <pablodhe@ext.inditex.com>
2022-03-21 19:13:58 +0100

    added cool animation to LoaderButton

--- src/components/buttons/LoaderButton.tsx
+++ src/components/buttons/LoaderButton.tsx
@@ -1,39 +1,48 @@
 import { ButtonBase, CircularProgress, Typography } from "@mui/material";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 interface Props {
   onClick?: () => Promise<any>;
   label?: string;
   backgroundColor?: string;
+  delay?: number;
 }
 
 export const LoaderButton = ({
   onClick = () => Promise.resolve(undefined),
   label = "Button",
-  backgroundColor,
+  backgroundColor = "#4692f9",
+  delay = 500,
 }: Props) => {
   const [isLoading, setIsLoading] = useState(false);
 
   return (
-    <ButtonBase
-      onClick={() => {
-        setIsLoading(true);
-        onClick().finally(() => setIsLoading(false));
-      }}
+    <motion.div
       style={{
-        padding: 13,
+        backgroundColor: backgroundColor,
         borderRadius: 10,
-        backgroundColor: backgroundColor ?? "#4692f9",
-        color: "white",
-        height: 50,
-        width: "100%",
       }}
+      whileTap={{ scale: 0.95 }}
     >
-      {isLoading ? (
-        <CircularProgress style={{ color: "white" }} size={25} />
-      ) : (
-        <Typography sx={{ color: "white" }}>{label}</Typography>
-      )}
-    </ButtonBase>
+      <ButtonBase
+        onClick={() => {
+          setIsLoading(true);
+          setTimeout(() => onClick().finally(() => setIsLoading(false)), delay);
+        }}
+        style={{
+          padding: 13,
+          color: "white",
+          height: 50,
+          width: "100%",
+        }}
+      >
+        {isLoading ? (
+          <CircularProgress style={{ color: "white" }} size={25} />
+        ) : (
+          <Typography sx={{ color: "white" }}>{label}</Typography>
+        )}
+      </ButtonBase>
+    </motion.div>
   );
 };
