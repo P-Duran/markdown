345666019143ad6088f536a0487e3031ff26e447
Pablo Durán <pablodhe@ext.inditex.com>
2022-04-06 20:30:40 +0200

    Merge branch 'alternative-navbar' of https://github.com/P-Duran/markdown into alternative-navbar

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,14 @@
 import { Grid, Box } from "@mui/material";
 import { ReactElement } from "react";
 import { markdownMock } from "src/mocks/markdownMocks";
-import { AlternativePreview } from "src/components/markdownPreview/AlternativePreview";
+import { MarkdownWorkspace } from "src/components/markdownWorkspace/MarkdownWorkspace";
 
 export const Dashboard = (): ReactElement => {
   return (
     <Box>
-      <Grid container spacing={3} direction="row" sx={{padding: 3}}>
+      <Grid container spacing={3} direction="row" sx={{ padding: 3 }}>
         {markdownMock.map((markdownValue, index) => (
-          <AlternativePreview markdownValue={markdownValue} index={index} />
+          <MarkdownWorkspace markdownValue={markdownValue} index={index} />
         ))}
       </Grid>
     </Box>
